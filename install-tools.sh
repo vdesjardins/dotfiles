bb8b960ff20aa9390bbfd272ced1150f168d8f7f
Vincent Desjardins <vdesjardins@gmail.com>
2019-03-17 10:28:58 -0400

    feat(install): add language servers

--- install-tools.sh
+++ install-tools.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-function install_tools() {
+install_tools() {
         install_hostess
         install_rg
         install_sh_formatter
@@ -9,7 +9,7 @@ function install_tools() {
         install_grv
 }
 
-function install_hostess() {
+install_hostess() {
         local tmp_dir=$(mktemp -d)
         curl -L https://github.com/cbednarski/hostess/releases/download/v0.3.0/hostess_linux_amd64 >${tmp_dir}/hostess
         sudo cp ${tmp_dir}/hostess /usr/local/bin/hostess
@@ -17,14 +17,14 @@ function install_hostess() {
         rm -rf ${tmp_dir}
 }
 
-function install_rg() {
+install_rg() {
         local tmp_dir=$(mktemp -d)
         curl -L https://github.com/BurntSushi/ripgrep/releases/download/0.7.1/ripgrep-0.7.1-x86_64-unknown-linux-musl.tar.gz | tar xzf - -C ${tmp_dir} --strip-components=1
         sudo cp ${tmp_dir}/rg /usr/local/bin/rg
         rm -rf ${tmp_dir}
 }
 
-function install_sh_formatter() {
+install_sh_formatter() {
         local tmp_dir=$(mktemp -d)
         curl -L https://github.com/mvdan/sh/releases/download/v2.4.0/shfmt_v2.4.0_linux_amd64 >${tmp_dir}/shfmt
         chmod +x ${tmp_dir}/shfmt
@@ -32,15 +32,23 @@ function install_sh_formatter() {
         rm -rf ${tmp_dir}
 }
 
-function install_pkgs() {
+install_pkgs() {
         sudo apt install -y highlight htop glances exuberant-ctags
 }
 
-function vim_langservers() {
-        sudo npm i -g -unsafe-perm bash-language-server
+vim_langservers() {
+        npm install -g -unsafe-perm bash-language-server
+
+        npm install -g dockerfile-language-server-nodejs
+
+        luarocks install --server=http://luarocks.org/dev lua-lsp
+        luarocks install luacheck
+        luarocks install lcf
+
+        go get -u github.com/saibing/bingo
 }
 
-function install_grv() {
+install_grv() {
         wget -O grv https://github.com/rgburke/grv/releases/download/v0.2.0/grv_v0.2.0_linux64
         chmod +x grv
         sudo mv grv /usr/local/bin/grv
