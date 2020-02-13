6fff427d9486d95001750c3cd824b09e7ab57d1a
Vincent Desjardins <vdesjardins@gmail.com>
2020-02-13 06:33:41 -0500

    feat: remove and add some zsh plugins

--- zsh/packages.zsh
+++ zsh/packages.zsh
@@ -14,16 +14,19 @@ zplug "zsh-users/zsh-syntax-highlighting", from:github, defer:3
 zplug "zsh-users/zsh-history-substring-search"
 zplug "zsh-users/zsh-completions"
 
-zplug "plugins/systemd", from:oh-my-zsh, defer:2
 zplug "plugins/docker", from:oh-my-zsh, defer:2
-zplug "plugins/dnf", from:oh-my-zsh, defer:2
 zplug "plugins/golang", from:oh-my-zsh, defer:2
 zplug "plugins/kubectl", from:oh-my-zsh, defer:2
 zplug "plugins/tmux", from:oh-my-zsh, defer:2
 zplug "plugins/git-flow", from:oh-my-zsh, defer:2
 zplug "plugins/git", from:oh-my-zsh, defer:2
 zplug "plugins/git-extras", from:oh-my-zsh, defer:3
+zplug "plugins/brew", from:oh-my-zsh, defer:2, if:"[[ $OSTYPE == *darwin* ]]"
+zplug "plugins/ansible", from:oh-my-zsh, defer:2
 zplug "plugins/vault", from:oh-my-zsh, defer:2
+zplug "plugins/helm", from:oh-my-zsh, defer:2
+zplug "plugins/terraform", from:oh-my-zsh, defer:2
+zplug "plugins/gcloud", from:oh-my-zsh, defer:2
 
 zplug "junegunn/fzf-bin", from:gh-r, as:command, rename-to:fzf
 
@@ -32,4 +35,4 @@ zplug "junegunn/fzf-bin", from:gh-r, as:command, rename-to:fzf
 #zplug "bhilburn/powerlevel9k", use:powerlevel9k.zsh-theme
 # zplug "afrozalm/agnosterAfro", as:theme
 # zplug "themes/agnoster", from:oh-my-zsh
-zplug denysdovhan/spaceship-prompt, use:spaceship.zsh, from:github, as:theme
+zplug "denysdovhan/spaceship-prompt", use:spaceship.zsh, from:github, as:theme
