88e0aa5495eb1c1c5068eb598388a8c2471aaedb
Vincent Desjardins <vdesjardins@gmail.com>
2020-11-08 21:23:43 -0500

    feat: add symlink to Applications when darwin

--- nix-home/program/alacritty/default.nix
+++ nix-home/program/alacritty/default.nix
@@ -1,8 +1,11 @@
-{ config, lib, pkgs, ... }:
-{
-  programs.alacritty = {
-    enable = true;
-  };
+{ config, lib, pkgs, ... }: {
+  programs.alacritty = { enable = true; };
 
   xdg.configFile."alacritty/alacritty.yml".source = ./alacritty.yml;
+
+  # home-manager do not yet symlink to ~/Applications
+  # https://github.com/nix-community/home-manager/issues/1341
+  home.file."Applications/Alacritty.app".source =
+    lib.mkIf pkgs.stdenv.hostPlatform.isDarwin
+    "${pkgs.alacritty}/Applications/Alacritty.app";
 }
