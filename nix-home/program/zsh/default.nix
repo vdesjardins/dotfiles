898cd6a1b84773092d2d97e95efb5db2d073717e
Vincent Desjardins <vdesjardins@gmail.com>
2020-11-19 20:34:39 +0000

    feat(zsh): base config

--- nix-home/program/zsh/default.nix
+++ nix-home/program/zsh/default.nix
@@ -1,6 +1,16 @@
 { config, pkgs, ... }:
+
 {
+  home.packages = with pkgs; [
+    zsh-autosuggestions
+    zsh-completions
+    zsh-history-substring-search
+    zsh-syntax-highlighting
+  ];
+
   programs.zsh = {
     enable = true;
+
+    enableAutosuggestions = true;
   };
 }
