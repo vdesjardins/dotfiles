c7a888bd2c37ff35967eaed4ae97a3dc5839d8aa
Vincent Desjardins <vdesjardins@gmail.com>
2020-11-19 20:35:05 +0000

    feat(just): zsh config

--- nix-home/program/just/default.nix
+++ nix-home/program/just/default.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }: {
   home.packages = with pkgs; [ just ];
 
+  programs.zsh.initExtra = ''
+    source <(just --completions zsh)
+  '';
+
   programs.fish.shellInit = ''
     just --completions fish | source
   '';
