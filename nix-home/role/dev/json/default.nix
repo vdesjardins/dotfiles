2d92c11d386816c71ab7559fc191fe982ff33357
Vincent Desjardins <vdesjardins@gmail.com>
2020-12-05 13:05:32 -0500

    feat(json): add support packages

--- nix-home/role/dev/json/default.nix
+++ nix-home/role/dev/json/default.nix
@@ -1,5 +1,7 @@
 { config, lib, pkgs, ... }:
 {
+  home.packages = with pkgs; [ nodePackages.prettier nodePackages.vscode-json-languageserver-bin ];
+
   programs.neovim = {
     plugins = with pkgs.vimPlugins; [
       coc-json
