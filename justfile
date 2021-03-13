3377932c9abe7a1e0bc41e08db2eddf94a81b087
Vincent Desjardins <vdesjardins@gmail.com>
2021-03-13 15:54:38 -0500

    feat(nix-darwin): initial commit

--- justfile
+++ justfile
@@ -27,7 +27,7 @@ home-manager:
 # home-manager install
 home-manager-install:
 	#!/usr/bin/env bash
-	. /home/vince/.nix-profile/etc/profile.d/nix.sh
+	. $HOME/.nix-profile/etc/profile.d/nix.sh
 	export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
 	nix-shell '<home-manager>' -A install
 
@@ -53,6 +53,25 @@ nix-cleanup:
 	#!/usr/bin/env bash
 	rm -rf $HOME/{.nix-channels,.nix-defexpr,.nix-profile,.config/nixpkgs}
 	sudo rm -rf /nix
+
+# darwin apply
+nix-darwin:
+	#!/usr/bin/env bash
+	darwin-rebuild switch
+
+# nix-darwin install
+nix-darwin-install:
+	#!/usr/bin/env bash
+	mkdir -p $HOME/.nixpkgs
+	ln -s $HOME/dotfiles/nix-darwin/darwin-configuration.nix $HOME/.nixpkgs/darwin-configuration.nix
+	nix-build https://github.com/LnL7/nix-darwin/archive/master.tar.gz -A installer
+	./result/bin/darwin-installer
+
+# nix-darwin update
+nix-darwin-update:
+	#!/usr/bin/env bash
+	nix-channel --update darwin
+	darwin-rebuild changelog
 
 # Local Variables:
 # mode: makefile
---
+++ nix-darwin/darwin-configuration.nix
@@ -0,0 +1,73 @@
+{ config, pkgs, ... }:
+
+{
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = [ pkgs.vim ];
+
+  # Use a custom configuration.nix location.
+  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
+  # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
+
+  # Auto upgrade nix package and the daemon service.
+  # services.nix-daemon.enable = true;
+  # nix.package = pkgs.nix;
+
+  # Create /etc/bashrc that loads the nix-darwin environment.
+  programs.zsh.enable = true; # default shell on catalina
+  # programs.fish.enable = true;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+
+  system.defaults = {
+    finder = {
+      AppleShowAllExtensions = true;
+      FXEnableExtensionChangeWarning = true;
+      _FXShowPosixPathInTitle = true;
+    };
+
+    # dock settings
+    dock = {
+      # auto show and hide dock
+      autohide = true;
+      # remove delay for showing dock
+      autohide-delay = "0.0";
+      # how fast is the dock showing animation
+      autohide-time-modifier = "1.0";
+      tilesize = 50;
+      static-only = false;
+      showhidden = false;
+      show-recents = false;
+      show-process-indicators = true;
+      orientation = "bottom";
+      mru-spaces = false;
+    };
+
+    NSGlobalDomain = {
+      AppleInterfaceStyle = "Dark";
+      "com.apple.sound.beep.feedback" = 0;
+      "com.apple.sound.beep.volume" = "0.000";
+      # allow key repeat
+      ApplePressAndHoldEnabled = false;
+      # delay before repeating keystrokes
+      InitialKeyRepeat = 10;
+      # delay between repeated keystrokes upon holding a key
+      KeyRepeat = 2;
+      AppleShowAllExtensions = true;
+      AppleShowScrollBars = "Automatic";
+    };
+  };
+
+  system.keyboard = {
+    enableKeyMapping = true;
+    remapCapsLockToControl = true;
+    swapLeftCommandAndLeftAlt = true;
+  };
+
+  fonts = {
+    enableFontDir = true;
+    fonts = with pkgs; [ (nerdfonts.override { fonts = [ "FiraCode" ]; }) ];
+  };
+}
