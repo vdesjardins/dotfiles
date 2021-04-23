3c40156e638dc4e97bf0fddb00d1cc9b434aaa52
Vincent Desjardins <vdesjardins@gmail.com>
2021-04-23 01:54:41 +0000

    feat(yaml): add yamllint

--- nix-home/role/dev/yaml/default.nix
+++ nix-home/role/dev/yaml/default.nix
@@ -1,12 +1,5 @@
-{ config, lib, pkgs, ... }:
-{
-  home.packages = with pkgs; [
-    nodePackages.yaml-language-server
-  ];
+{ config, lib, pkgs, ... }: {
+  home.packages = with pkgs; [ nodePackages.yaml-language-server yamllint ];
 
-  programs.neovim = {
-    plugins = with pkgs.vimPlugins; [
-      coc-yaml
-    ];
-  };
+  programs.neovim = { plugins = with pkgs.vimPlugins; [ coc-yaml ]; };
 }
