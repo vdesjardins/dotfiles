684bc2df6d13dc120f97de60a6f46d301aac3495
Vincent Desjardins <vdesjardins@gmail.com>
2021-04-23 01:56:07 +0000

    feat(docker): add hadolint

--- nix-home/role/ops/docker/default.nix
+++ nix-home/role/ops/docker/default.nix
@@ -1,14 +1,15 @@
-{ config, lib, pkgs, ... }:
-{
+{ config, lib, pkgs, ... }: {
   home.packages = with pkgs; [
     buildkit
     dive
     nodePackages.dockerfile-language-server-nodejs
+    hadolint
   ];
 
   programs.neovim = {
-    plugins = with pkgs.vimPlugins; [
-      # Dockerfile-vim
-    ];
+    plugins = with pkgs.vimPlugins;
+      [
+        # Dockerfile-vim
+      ];
   };
 }
