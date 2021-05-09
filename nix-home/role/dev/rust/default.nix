23e9f9aae429ebe4d2c8f60a12862a2075a118bf
Vincent Desjardins <vdesjardins@gmail.com>
2021-05-09 02:46:08 +0000

    feat(rust): add clippy

--- nix-home/role/dev/rust/default.nix
+++ nix-home/role/dev/rust/default.nix
@@ -1,14 +1,7 @@
-{ config, lib, pkgs, ... }:
-{
-  home.packages = with pkgs; [
-    rustup
-    rust-analyzer
-  ];
+{ config, lib, pkgs, ... }: {
+  home.packages = with pkgs; [ rustup rust-analyzer crate2nix clippy ];
 
   programs.neovim = {
-    plugins = with pkgs.vimPlugins; [
-      coc-rust-analyzer
-      rust-vim
-    ];
+    plugins = with pkgs.vimPlugins; [ coc-rust-analyzer rust-vim ];
   };
 }
